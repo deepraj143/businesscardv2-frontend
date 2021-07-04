fb12e4816bc38d918d958336c281ab8ee9cf3139
deepraj <deeprajgupta143@gmail.com>
2021-07-04 23:19:57 +0530

    build

--- src/Context/PrefixContext.js
+++ src/Context/PrefixContext.js
@@ -5,7 +5,7 @@ export const PrefixContext=createContext();
 
 const Prefixcontext = ({children}) => {
     
-    const prefixurl="http://localhost:5000"
+    const prefixurl="https://business-card-generator.netlify.app"
     
     return (
         <PrefixContext.Provider value={{prefixurl}}>
